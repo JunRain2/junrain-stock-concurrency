baced16ca4ae9a94d72d1979832d20d8ca61972b
JunRain <sa2313@tukorea.ac.kr>
2025-11-18 18:02:19 +0900

    feat: 모니터링 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,11 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.14.5")
+
     runtimeOnly("com.mysql:mysql-connector-j")
-    
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
